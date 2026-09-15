2ee1b7311f6daf5829263a4be5a8d3e3ccb6dd13
Mark <mrhappyasthma@gmail.com>
2026-09-15 14:02:10 -0700

    Initial project setup.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+
+int main(int argc, char* argv[]) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL_Init failed: %s", SDL_GetError());
+        return -1;
+    }
+    
+    SDL_Log("Hello world\n");
+    
+    SDL_Quit();
+    
+    return 0;
+}
